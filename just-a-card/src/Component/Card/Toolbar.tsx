d9e59547b53bbc62d5e8577dea47328e270299f7
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2023-11-29 14:50:11 +0000

    Fix just-a-card.

--- just-a-card/src/Component/Card/Toolbar.tsx
+++ just-a-card/src/Component/Card/Toolbar.tsx
@@ -1,6 +1,6 @@
 export const Toolbar: React.FC = () => (
 	<section className='card__toolbar'>
-		<button id='addToCart' type="button" children={Add to cart} />
-		<button id='buyNow' type="button" children={Buy now} />
+		<button id='addToCart' type="button" children={'Add to cart'} />
+		<button id='buyNow' type="button" children={'Buy now'} />
 	</section>
 )
