5c1a4cb2f95da5d3cdfb0e24132ef6aaadde5aa5
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-01-08 06:04:28 +0000

    Upload 02. Ensure * does not have trailing whitespaces.
    #repository
    
    Changes to be committed:
    	- renamed:    script/00-format('package-lock.json').py -> script/python/00-format('package-lock.json').py
    	- renamed:    script/01-ensure('*', 'does not have leading spaces').py -> script/python/01-ensure('*', 'does not have leading spaces').py
    	- renamed:    script/pre-commit.py -> script/python/pre-commit.py
    	- new file:   "script/swift/00-format(\"package-lock.json\").swift"
    	- new file:   "script/swift/01-ensure(\"*\", \"does not have leading spaces\").swift"
    	- new file:   "script/swift/02-ensure(\"*\", \"does not have trailing whitespaces\").swift"
    	- new file:   script/swift/essentials.swift

---
+++ script/swift/02-ensure("*", "does not have trailing whitespaces").swift
@@ -0,0 +1,19 @@
+/* Imported at 11:00 AM on Mon 8 Jan 2024.
+*/
+let subpaths = FileManager.default.subpaths(atPath: ".") ?? []
+
+/* A file _does not have to_ include a line ~~ending with a space character~~!
+*/
+let condition: (String) -> Bool = {
+	let lines = $0.split(separator: "\n")
+	let booleans = lines.map { line in !line.hasSuffix(" ") }
+	return !booleans.includes(false)
+}
+
+execute(subpaths: subpaths, satisfies: condition) { subpath in
+	let suffixes = [".ts", ".tsx"]
+	let booleans = suffixes.map { suffix in
+		subpath.hasSuffix(suffix)
+	}
+	return booleans.includes(true)
+}
