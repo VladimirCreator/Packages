0d89c2b2dc4fcc5fc90b38c2ae46bb8a2c7c8e24
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-01-17 22:02:37 +0000

    tailwind.config.js

--- _r__-for-a-comment/tailwind.config.js
+++ _r__-for-a-comment/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'index.html', 'src/**/*.{jsx}'
+		'./index.html', './src/**/*.{jsx}'
 	],
 	theme:{extend:{},plugins:[]}
 }
