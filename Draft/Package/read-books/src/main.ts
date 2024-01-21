40f64d896029bc348831b441a0eff72d7e22c1c3
Vladimir <49140851+VladimirCreator@users.noreply.github.com>
2024-01-21 21:33:39 +0000

    嵐

--- Draft/Package/read-books/src/main.ts
+++ Draft/Package/read-books/src/main.ts
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' /* @VladimirCreator added this at 2:28 AM on Mon 22 Jan 2024. */
 
 import { App } from './component/App'
 import './index.css'
+
+const router = createBrowserRouter([
+	{
+		path: '/', element: React.createElement(App)
+	}
+])
 
 const main: HTMLElement|null = document.querySelector('main#root')
 const root = main ? ReactDOM.createRoot(main) : undefined
@@ -10,6 +17,8 @@ const root = main ? ReactDOM.createRoot(main) : undefined
 root?.render(
 	React.createElement(
 		React.StrictMode, null,
-		React.createElement(App)
+		React.createElement(
+			RouterProvider, { router }
+		)
 	)
 )
